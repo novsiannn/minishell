e008fc5b5bcc366e211b68353ddda034013c04ab
novsiann <nikitaovsyannik77@gmail.com>
2023-07-26 22:07:06 +0200

    first commit

---
+++ inc/minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nikitos <nikitos@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 19:31:56 by nikitos           #+#    #+#             */
+/*   Updated: 2023/07/26 22:00:38 by nikitos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/libft.h"
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+
+#endif 

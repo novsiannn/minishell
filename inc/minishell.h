5758d5f6904da52fd8633b91f011fc99909d678f
Illia Khristinchenko <ikhristi@c2s8d26.42wolfsburg.de>
2023-07-31 16:17:48 +0200

    defines

--- inc/minishell.h
+++ inc/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nikitos <nikitos@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ikhristi <ikhristi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 19:31:56 by nikitos           #+#    #+#             */
-/*   Updated: 2023/07/26 22:00:38 by nikitos          ###   ########.fr       */
+/*   Updated: 2023/07/31 16:15:16 by ikhristi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,18 @@
 # include <readline/history.h>
 # include <stdlib.h>
 
-#endif 
+# define WORD 0
+# define PIPE 1 // |
+# define SPACE 2 // ' '
+# define GREATER_THAN 3 // >
+# define LESS_THAN 4 // <
+# define DOUBLE_QUOTES 5 // "
+# define SINGLE_QUOTES 6 // '
+# define HEREDOCK 7 // <<
+# define APPEND 8 // >>
+# define EXPANSION 9 
+# define MINI_FILE 10
+
+
+
+#endif

ab6203c313fbfc8beeb415b730f6e9e76599d1ba
Nikita Ovsiannik <novsiann@c2s7d20.42wolfsburg.de>
2023-08-01 00:12:12 +0200

    split words by spaces and tabs

--- src/lexer.c
+++ src/lexer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ikhristi <ikhristi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: novsiann <novsiann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 16:23:45 by novsiann          #+#    #+#             */
-/*   Updated: 2023/07/31 22:56:22 by ikhristi         ###   ########.fr       */
+/*   Updated: 2023/08/01 00:11:20 by novsiann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,8 @@
 
 void	cut_word(char *str, int start, int end)
 {
-	while (*str && start <= end)
-	{
-		write(1, &str[start], 1);
-		start++;
-	}
+	while (*str && start < end)
+		write(1, &str[start++], 1);
 }
 
 void	lexer(char *input)//
@@ -32,14 +29,15 @@ void	lexer(char *input)//
 	// type = get_type(input[i]);
 	while (input[start])
 	{
-		while (input[start] >= 8 && input[start] <= 14 && input[start] == 32 &&input[start] != '\0')
+		while (((input[start] >= 8 && input[start] <= 14) || input[start] == 32) && input[start] != '\0')
 			start++;
 		end = start;
-		while (!(input[end] >= 8 && input[end] <= 14) && input[start] != 32 && input[end] != '\0')
+		while (!(input[start] >= 8 && input[start] <= 14) && input[end] != ' '  && input[end] != '\0')
 			end++;
 		// printf("%d , %d", start, end);
 		cut_word(input, start, end);
 		start = end;
 	}
+	write(1, "\n", 1);
 	// return (list);
 }
